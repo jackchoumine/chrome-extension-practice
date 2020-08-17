316f9b2fa4e00124e6002bbd9bd9899d4c5b832d
jackzhoumine <jackzhoumine@163.com>
2020-08-18 00:44:02 +0800

    增加js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,18 @@
  * @Description: 
  * @Date: 2020-08-17 22:54:22 +0800
  * @Author: JackChouMine
- * @LastEditTime: 2020-08-17 23:39:50 +0800
+ * @LastEditTime: 2020-08-18 00:39:33 +0800
  * @LastEditors: JackChouMine
  */
-console.log('我的第一个扩展')
-// alert('hello extension')
+function my_clock (el) {
+  const today = new Date()
+  const h = today.getHours()
+  let m = today.getMinutes()
+  let s = today.getSeconds()
+  m = m < 10 ? `0${m}` : m
+  s = s < 10 ? `0${s}` : s
+  el.innerHTML = `${h}:${m}:${s}`
+  setTimeout(() => { my_clock(el) }, 1000, el)
+}
+const clockDiv = document.getElementById('clock-div')
+my_clock(clockDiv)
